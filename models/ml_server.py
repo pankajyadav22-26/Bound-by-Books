78f7f53d9d42f996c8cf3d6a80360ab44bdf77bd
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2025-08-16 22:44:45 +0530

    updated frontend_url

--- models/ml_server.py
+++ models/ml_server.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bookstore2943.netlify.app"],  # frontend URL
+    allow_origins=["https://boundbybooks29.netlify.app"],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
