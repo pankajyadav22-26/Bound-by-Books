30fc1ebfe1dedf067f0c620729850fbfce8a66d3
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2025-08-16 18:51:11 +0530

    update

--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -1,16 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
+    const serviceToken = req.headers["x-api-key"];
+    if (serviceToken && serviceToken === process.env.SERVICE_TOKEN) {
+        return next();
+    }
+
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
-    if (token == null) {
+    if (!token) {
         return res.status(401).json({ message: "Authentication token required" });
     }
 
     jwt.verify(token, "bookstore123", (err, user) => {
         if (err) {
-            return res.status(403).json({message: "Token expired. Please signIn again"});
+            return res.status(403).json({ message: "Token expired. Please sign in again" });
         }
         req.user = user;
         next();
