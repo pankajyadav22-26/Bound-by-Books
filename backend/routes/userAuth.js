11e3b0d1cc17356ce284552e3162a5395a836e9c
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2025-08-16 19:17:06 +0530

    updated

--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -1,10 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-    console.log("Authenticating user...");
     const serviceToken = req.headers["x-api-key"];
     if (serviceToken && serviceToken === process.env.SERVICE_TOKEN) {
-        console.log("Service token provided, skipping authentication");
         return next();
     }
 
